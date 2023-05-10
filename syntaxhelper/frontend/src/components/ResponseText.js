78a39842725cbfe761fad64f01352efb3e8a16ed
realbcole <realbcole@gmail.com>
2023-05-10 18:42:41 -0400

    Initial commit

---
+++ syntaxhelper/frontend/src/components/ResponseText.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactMarkdown from 'react-markdown';
+
+// Formats the response text
+const ResponseText = ({ responseText }) => {
+    // Find code sections enclosed in triple backticks
+    const codeSections = responseText.match(/```[\s\S]+?```/g);
+
+    if (codeSections) {
+        codeSections.forEach(codeSection => {
+            // Replace code sections with formatted Markdown code blocks
+            responseText = responseText.replace(codeSection, `\n\n${codeSection
+                }\n\n`);
+        });
+    }
+    return (
+        <ReactMarkdown
+            children={responseText}
+            components={{
+                code({ node, inline, className, children, ...props }) {
+                    return (
+                        <code className="text-primaryDark" {...props}>
+                            {children}
+                        </code>
+                    );
+                },
+            }}
+        />
+    );
+};
+export default ResponseText;
